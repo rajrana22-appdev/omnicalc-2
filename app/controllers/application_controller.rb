1f6e8dcff6af70ca7c715a1d7ff7e9b82c003f9c
Raj Rana <rajrana22@uchicago.edu>
2022-04-23 18:45:04 +0000

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,20 @@ class ApplicationController < ActionController::Base
   def divide
     render({ :template => "operations/divide.html.erb" })
   end
+
+  def wiz_add
+    render({ :template => "calculations/wiz_add.html.erb" })
+  end
+
+  def wiz_subtract
+    render({ :template => "calculations/wiz_subtract.html.erb" })
+  end
+
+  def wiz_multiply
+    render({ :template => "calculations/wiz_multiply.html.erb" })
+  end
+
+  def wiz_divide
+    render({ :template => "calculations/wiz_divide.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get("/subtract", { :controller => "application", :action => "subtract" })
   get("/multiply", { :controller => "application", :action => "multiply" })
   get("/divide", { :controller => "application", :action => "divide" })
+  get("/wizard_add", { :controller => "application", :action => "wiz_add" })
+  get("/wizard_subtract", { :controller => "application", :action => "wiz_subtract" })
+  get("/wizard_multiply", { :controller => "application", :action => "wiz_multiply" })
+  get("/wizard_divide", { :controller => "application", :action => "wiz_divide" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
